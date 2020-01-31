62563b634375157b05dca7aa2e1eece09d7c365f
innocent lou <bisonlou@gmail.com>
2020-01-31 09:55:25 +0300

    added a configurable host

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+
 from api import app
+from os import environ
+from werkzeug.serving import run_simple
+
+
+host = environ['HOST']
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    run_simple(host, 8080, app, use_reloader=False)
