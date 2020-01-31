bcd1964c6ab0b8e50277ff750fba630d1c29abb5
innocent lou <bisonlou@gmail.com>
2020-01-31 14:49:12 +0300

    added werkzeug server

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+import os
 from api import app
+from werkzeug.serving import run_simple
+from dotenv import load_dotenv
+
+load_dotenv()
+host = os.getenv("HOST")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    run_simple(host, 8080, app, use_reloader=False)
