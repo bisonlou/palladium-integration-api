ea184df511c2ccb08f77a91af95256eb23c5f914
innocent lou <bisonlou@gmail.com>
2020-02-05 15:02:29 +0300

    added user registration

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from werkzeug.serving import run_simple
 from dotenv import load_dotenv
 
 load_dotenv()
-host = os.getenv("HOST")
+host = os.getenv('HOST')
+environment = os.getenv('FLASK_ENV')
 
-
-if __name__ == "__main__":
-    run_simple(host, 8080, app, use_reloader=False)
+if __name__ == '__main__':
+    if environment == 'production':
+        run_simple(host, 8080, app, use_reloader=False)
+    else:
+        app.run(debug=True)
