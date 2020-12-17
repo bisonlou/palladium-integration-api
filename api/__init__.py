68b84fe1173e1115d4beb6e54b1c6db559f5d550
innocent lou <bisonlou@gmail.com>
2020-12-17 18:26:28 +0300

    activated finance module

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from api.database import create_db
-# from api.database.finance_services import cursor
+from api.database.finance_services import cursor
 
 app = Flask(__name__)
 CORS(app)
@@ -12,7 +12,7 @@ create_db(app, database_path)
 
 from api.controllers.users import user_module
 from api.controllers.errors import errorhandler
-# from api.controllers.journal import journal_module
+from api.controllers.journal import journal_module
 from api.controllers.stationery import stationery_module
 from api.controllers.stationery_requisition import (
     stationery_requisition_module,
@@ -24,7 +24,7 @@ from api.controllers.health import health_check
 health_check(app)
 user_module(app)
 errorhandler(app)
-# journal_module(app)
+journal_module(app)
 stationery_module(app)
 stationery_requisition_module(app)
 project_module(app)
